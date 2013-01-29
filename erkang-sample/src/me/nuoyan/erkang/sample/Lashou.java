4295edf830bb649a5a93f47ca2e6379df81249c7
Joshua Zhang <zyh0532@gmail.com>
2013-01-29 16:15:45 +0800

    sample
    Use a website as a sample to show how to configure a schedule and start
    it.

---
+++ erkang-sample/src/me/nuoyan/erkang/sample/Lashou.java
@@ -0,0 +1,15 @@
+package me.nuoyan.erkang.sample;
+
+public class Lashou {
+	
+	private String merchantName;
+
+	public String getMerchantName() {
+		return merchantName;
+	}
+
+	public void setMerchantName(String merchantName) {
+		this.merchantName = merchantName;
+	}
+
+}
